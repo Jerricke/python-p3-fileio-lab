17d349218fa3df51ad25750473f02b0b21e98a40
Jerricke <eejerrickw@gmail.com>
2023-07-27 09:49:00 -0600

    completo

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as newFile:
+        newFile.write(file_content)
     pass
+
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as newFile:
+        newFile.write(append_content)
     pass
 
+
 def read_file(file_name):
+    with open(f"{file_name}.txt", encoding="utf-8") as newFile:
+        return newFile.read()
     pass
